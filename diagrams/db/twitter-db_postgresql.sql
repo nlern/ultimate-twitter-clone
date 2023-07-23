17d64ed24ebe6b4840506b940302e36202a028c2
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2023-07-23 20:03:43 +0530

    chore: update Supabase sql script

--- diagrams/db/twitter-db_postgresql.sql
+++ diagrams/db/twitter-db_postgresql.sql
@@ -1,80 +1,93 @@
-CREATE TABLE "user" (
-  "id" uuid PRIMARY KEY,
-  "username" string,
-  "email" string
+-- Create a table for public profiles
+create table profiles (
+  id uuid references auth.users on delete cascade not null primary key,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  username text unique,
+  full_name text,
+
+  constraint username_length check (char_length(username) >= 3)
+);
+-- Set up Row Level Security (RLS)
+-- See https://supabase.com/docs/guides/auth/row-level-security for more details.
+alter table profiles
+  enable row level security;
+
+create policy "Public profiles are viewable by everyone." on profiles
+  for select using (true);
+
+create policy "Users can insert their own profile." on profiles
+  for insert with check (auth.uid() = id);
+
+create policy "Users can update own profile." on profiles
+  for update using (auth.uid() = id);
+
+-- This trigger automatically creates a profile entry when a new user signs up via Supabase Auth.
+-- See https://supabase.com/docs/guides/auth/managing-user-data#using-triggers for more details.
+create function public.handle_new_user()
+returns trigger as $$
+begin
+  insert into public.profiles (id, username)
+  values (new.id, new.raw_user_meta_data->>'username');
+  return new;
+end;
+$$ language plpgsql security definer;
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
+
+CREATE TABLE tweets (
+  id UUID PRIMARY KEY,
+  text text not null,
+  profile_id UUID not null,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE
 );
 
-CREATE TABLE "tweet" (
-  "id" uuid PRIMARY KEY,
-  "text" string,
-  "authorId" string,
-  "createdAt" datetime,
-  "updatedAt" datetime
+
+CREATE TABLE hashtags (
+  id UUID PRIMARY KEY,
+  name text not null
 );
 
-CREATE TABLE "hashtag" (
-  "id" uuid PRIMARY KEY,
-  "name" string
+CREATE TABLE tweet_hashtag (
+  tweet_id UUID not null,
+  hashtag_id UUID not null, 
+  PRIMARY KEY (tweet_id, hashtag_id),
+  FOREIGN KEY (tweet_id) REFERENCES tweets(id) ON DELETE CASCADE,
+  FOREIGN KEY (hashtag_id) REFERENCES hashtags(id) ON DELETE CASCADE
 );
 
-CREATE TABLE "reply" (
-  "id" uuid PRIMARY KEY,
-  "text" string,
-  "userId" string,
-  "tweetId" string,
-  "replyId" string
+
+CREATE TABLE replies (
+  id UUID PRIMARY KEY,
+  text text not null,
+  user_id UUID not null,
+  tweet_id UUID,
+  reply_id UUID,
+  FOREIGN KEY (user_id) REFERENCES profiles(id) ON DELETE CASCADE,
+  FOREIGN KEY (tweet_id) REFERENCES tweets(id) ON DELETE CASCADE,
+  FOREIGN KEY (reply_id) REFERENCES replies(id) ON DELETE CASCADE
 );
 
-CREATE TABLE "like" (
-  "id" uuid PRIMARY KEY,
-  "userId" string,
-  "tweetId" string,
-  "createdAt" datetime
+
+CREATE TABLE "likes" (
+  id UUID PRIMARY KEY,
+  user_id UUID not null,
+  tweet_id UUID not null,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  CONSTRAINT like_unique UNIQUE (user_id, tweet_id),
+  FOREIGN KEY (user_id) REFERENCES profiles(id) ON DELETE CASCADE,
+  FOREIGN KEY (tweet_id) REFERENCES tweets(id) ON DELETE CASCADE
 );
 
-CREATE TABLE "bookmark" (
-  "id" uuid PRIMARY KEY,
-  "userId" string,
-  "tweetId" string,
-  "createdAt" datetime
+CREATE TABLE bookmarks (
+  id UUID PRIMARY KEY,
+  user_id UUID,
+  tweet_id UUID,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  CONSTRAINT bookmark_unique UNIQUE (user_id, tweet_id),
+  FOREIGN KEY (user_id) REFERENCES profiles(id) ON DELETE CASCADE,
+  FOREIGN KEY (tweet_id) REFERENCES tweets(id) ON DELETE CASCADE
 );
-
-CREATE UNIQUE INDEX ON "like" ("userId", "tweetId");
-
-CREATE UNIQUE INDEX ON "bookmark" ("userId", "tweetId");
-
-CREATE TABLE "tweet_hashtag" (
-  "tweet_id" uuid,
-  "hashtag_id" uuid,
-  PRIMARY KEY ("tweet_id", "hashtag_id")
-);
-
-ALTER TABLE "tweet_hashtag" ADD FOREIGN KEY ("tweet_id") REFERENCES "tweet" ("id");
-
-ALTER TABLE "tweet_hashtag" ADD FOREIGN KEY ("hashtag_id") REFERENCES "hashtag" ("id");
-
-
-ALTER TABLE "tweet" ADD FOREIGN KEY ("authorId") REFERENCES "user" ("id");
-
-ALTER TABLE "reply" ADD FOREIGN KEY ("userId") REFERENCES "user" ("id");
-
-ALTER TABLE "reply" ADD FOREIGN KEY ("tweetId") REFERENCES "tweet" ("id");
-
-CREATE TABLE "reply_reply" (
-  "reply_id" uuid,
-  "reply_replyId" string,
-  PRIMARY KEY ("reply_id", "reply_replyId")
-);
-
-ALTER TABLE "reply_reply" ADD FOREIGN KEY ("reply_id") REFERENCES "reply" ("id");
-
-ALTER TABLE "reply_reply" ADD FOREIGN KEY ("reply_replyId") REFERENCES "reply" ("replyId");
-
-
-ALTER TABLE "like" ADD FOREIGN KEY ("userId") REFERENCES "user" ("id");
-
-ALTER TABLE "like" ADD FOREIGN KEY ("tweetId") REFERENCES "tweet" ("id");
-
-ALTER TABLE "bookmark" ADD FOREIGN KEY ("userId") REFERENCES "user" ("id");
-
-ALTER TABLE "bookmark" ADD FOREIGN KEY ("tweetId") REFERENCES "tweet" ("id");
