dd58975865b8c1bcd6d8cb10f8fa70a373dddf4d
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2023-07-27 21:28:48 +0530

    feat!(sidebar): Show user profile details in sidebar 1. Fetched and shown user details in sidebar

--- src/components/server/LeftSidebar.tsx
+++ src/components/server/LeftSidebar.tsx
@@ -1,6 +1,10 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 import Link from "next/link";
 import { BiBell, BiBookmark, BiEnvelope, BiHomeCircle, BiLogoTwitter, BiSearch, BiUser } from "react-icons/bi";
 import { BsThreeDots } from "react-icons/bs";
+
+import type { Database } from "@/lib/types/database.types";
 
 import { Button } from "@/components/ui/button";
 
@@ -35,7 +39,36 @@ const NAVIGATION_ITEMS = [
     }
 ]
 
-export default function LeftSidebar() {
+async function getUserDetails() {
+    const supabase = createServerComponentClient<Database>({ cookies });
+
+    const { data, error } = await supabase.auth.getUser();
+
+    if (error) {
+        console.log('Error: Failed to get user details.')
+        throw new Error('Failed to get user details.');
+    }
+
+    const userId = data.user.id;
+
+    const { data: userProfile, error: userProfileError } = await supabase.from('profiles')
+        .select('username, full_name').eq('id', userId)
+
+    if (userProfileError) {
+        console.log('Error: Failed to fetch user profile details.')
+        throw new Error('Failed to get user details.');
+    }
+
+    if (!userProfile || userProfile.length === 0) {
+        throw new Error('User profile does not exist.')
+    }
+
+    return userProfile[0];
+}
+
+export default async function LeftSidebar() {
+    const userDetails = await getUserDetails();
+
     return (
         <section className='sticky top-0 w-[270px] flex flex-col items-stretch justify-between h-screen space-y-4 p-4'>
             <div className="text-2xl">
@@ -61,10 +94,10 @@ export default function LeftSidebar() {
                         <div className="rounded-full bg-slate-400 w-10 h-10" />
                         <div className="text-left text-sm">
                             <div className="font-semibold">
-                                John Doe
+                                {userDetails.full_name}
                             </div>
                             <div className="text-gray-600">
-                                @johndoe
+                                @{userDetails.username}
                             </div>
                         </div>
                     </div>
